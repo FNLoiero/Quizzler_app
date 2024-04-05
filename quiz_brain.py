5dc827abd1331fc57dcea350251540d94d49dd0d
Federico Loiero <federico.loiero95@gmail.com>
2024-04-05 18:18:06 -0300

    limpieza de codigo

--- quiz_brain.py
+++ quiz_brain.py
@@ -4,7 +4,6 @@ class QuizBrain:
 
     def __init__(self, q_list):
         self.question_number = 0
-        self.score = 0
         self.question_list = q_list
         self.current_question = None
 
@@ -16,13 +15,10 @@ class QuizBrain:
         self.question_number += 1
         q_text = html.unescape(self.current_question.text)
         return f"Q.{self.question_number}: {q_text} (True/False): "
-        # user_answer = input(f"Q.{self.question_number}: {q_text} (True/False): ")
-        # self.check_answer(user_answer)
 
     def check_answer(self, user_answer):
         correct_answer = self.current_question.answer
         if user_answer.lower() == correct_answer.lower():
-            self.score += 1
             return True
         else:
             return False
